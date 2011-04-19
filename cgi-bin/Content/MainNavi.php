56c8faa0ba134a9b073479a03e625fedb91d3229
Gunnar Droege <mail@gunnardroege.de>
2011-04-19 23:33:36 +0100

    new files before france

---
+++ cgi-bin/Content/MainNavi.php
@@ -0,0 +1,46 @@
+<?php
+
+class MainNavi
+{
+	private $bgPic;
+	private $titlesAndUrls;
+	private $urlPrefix;
+	
+	public function MainNavi()
+	{
+		$this->urlPrefix = "/index/";
+	} 
+	
+	public function SetPic($pic)
+	{
+		$this->bgPic = $pic;
+	}
+	
+	public function SetTitles($titles)
+	{
+		$this->titlesAndUrls = $titles;
+	}
+	
+	public function Render()
+	{
+		$builder = ContentMgr::GetInstance()->GetBuilder();
+		
+		$menuDiv = $builder->AddTag("div", "menuLeft", NULL);
+		$titles = array_keys($this->titlesAndUrls);
+		PrintHtmlComment("Main navi render:".count($this->titlesAndUrls));
+		for($menuIndex = 0;$menuIndex<count($this->titlesAndUrls); $menuIndex++)
+		{
+			$linkNode = $builder->AddTag("a");
+			$link = $this->urlPrefix.$this->titlesAndUrls[$titles[$menuIndex]];
+			PrintHtmlComment($link);
+			$linkNode->setAttribute("href", $link);
+			$newEntry = $builder->AddTag("div", "mainNaviEntry_".$titles[$menuIndex], "mainNaviEntry", $titles[$menuIndex]);
+			$linkNode->appendChild($newEntry);
+		    $menuDiv->appendChild($linkNode);
+		}
+		$builder->GetDoc()->appendChild($menuDiv);
+		$builder->Render();
+	}
+}
+
+?>
