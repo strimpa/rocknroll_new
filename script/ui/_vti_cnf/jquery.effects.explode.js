96d010d5575e178d5bd0de02f824dad3436933bb
Gunnar Droege <mail@gunnardroege.de>
2011-04-30 02:08:58 +0100

    jquery ui

---
+++ script/ui/_vti_cnf/jquery.effects.explode.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 Apr 2011 15:20:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|Gunnar-PC\\Gunnar
+vti_modifiedby:SR|Gunnar-PC\\Gunnar
+vti_timecreated:TR|02 Apr 2011 15:20:00 -0000
+vti_cacheddtm:TX|02 Apr 2011 15:20:00 -0000
+vti_filesize:IR|2694
+vti_backlinkinfo:VX|
